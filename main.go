0d353aaf5ec0478d13e5bfa6da35cb0f9d4374c9
menaveen001 <menaveenpal@gmail.com>
2022-03-25 14:55:14 +0530

    update:- substraction

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 	addition()
-
+	substraction()
 }
 func addition() {
 
 	a, b := 2, 3
 	sum := a + b
 	fmt.Println(sum)
+}
+func substraction() {
+
+	a, b := 8, 5
+	sub := a - b
+	fmt.Println(sub)
 }
