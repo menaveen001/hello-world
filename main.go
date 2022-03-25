19c7a00d17d4ad0bc8de5f43d2c74641386ee17e
menaveen001 <menaveenpal@gmail.com>
2022-03-25 20:53:08 +0530

    asking user input

--- main.go
+++ main.go
@@ -31,11 +31,15 @@ func multipiction() {
 func getUserInput() {
 	var FirstName string
 	var LastName string
+	var age int
 
 	fmt.Println("Enter your first name")
 	fmt.Scan(&FirstName)
 
 	fmt.Println("Enter your last name")
 	fmt.Scan(&LastName)
+
+	fmt.Println("Enter your age")
+	fmt.Scan(&age)
 
 }
